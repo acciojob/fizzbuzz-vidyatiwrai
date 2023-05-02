d47430b3a9ae91f2cf56f3137c23772db24aaae5
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-05-02 10:11:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ 
+for(let i = 1;i<=20;i++){
+	if(i%3==0){
+		alert("Fizz");
+	}else if(i%5==0){
+		alert("Buzz");
+	}else if(i%3==0 && i%5==0){
+		alert("FizzBuzz");
+	}
+}
